ad64f3c52961a8b737c737ffa68523a561d98dc5
antonkomarev <ell@cybercog.su>
2019-12-21 06:05:26 +0300

    Add Comparable interface

---
+++ src/Comparable.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace I\Comparison;
+
+interface Comparable extends Equalable
+{
+    /**
+     * Get integer identifier result of comparison of $this and $that.
+     *
+     * @param \I\Comparison\Comparable $that
+     * @return int Returns 0 if $this is equal to $that
+     *             Returns -1 if $this is less than $that
+     *             Returns 1 if $this is greater than $that
+     *
+     * @throws \I\Comparison\Exceptions\ObjectsIncomparable
+     */
+    public function getComparisonTo(self $that): int;
+
+    /**
+     * If $this is greater than $that.
+     *
+     * @param \I\Comparison\Comparable $that
+     * @return bool
+     *
+     * @throws \I\Comparison\Exceptions\ObjectsIncomparable
+     */
+    public function isGreaterThan(self $that): bool;
+
+    /**
+     * If $this is greater than $that or equal to $that.
+     *
+     * @param \I\Comparison\Comparable $that
+     * @return bool
+     *
+     * @throws \I\Comparison\Exceptions\ObjectsIncomparable
+     */
+    public function isGreaterThanOrEqualTo(self $that): bool;
+
+    /**
+     * If $this is less than $that.
+     *
+     * @param \I\Comparison\Comparable $that
+     * @return bool
+     *
+     * @throws \I\Comparison\Exceptions\ObjectsIncomparable
+     */
+    public function isLessThan(self $that): bool;
+
+    /**
+     * If $this is less than $that or equal to $that.
+     *
+     * @param \I\Comparison\Comparable $that
+     * @return bool
+     *
+     * @throws \I\Comparison\Exceptions\ObjectsIncomparable
+     */
+    public function isLessThanOrEqualTo(self $that): bool;
+}
